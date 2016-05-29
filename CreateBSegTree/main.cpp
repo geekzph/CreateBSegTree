404ce7b1cfb26915f293781aa4722404e30b13ea
geekzph <geekzph@qq.com>
2016-05-29 09:11:48 +0800

    Initial Commit

---
+++ CreateBSegTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CreateBSegTree
+//
+//  Created by zph on 16/5/29.
+//  Copyright © 2016年 zph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
